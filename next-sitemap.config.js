0a13d4ef6ee251481e2d3be22055132daed4ace2
Jimmy Wong <37492595+thinkingjimmy@users.noreply.github.com>
2023-12-23 22:49:50 +0800

    Add next-sitemap dependency

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://comflowy.com',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
