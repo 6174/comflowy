31765abcf1996458f1d41f6e17d51ad7623542dd
Jimmy Wong <thinkingjimmy@outlook.com>
2024-01-25 00:10:30 +0800

    Update Model Recommadation Pages

---
+++ components/model-score-card.jsx
@@ -0,0 +1,7 @@
+export default function ModelScoreCard() {
+  return (
+    <div class="rounded border border-solid border-gray-300 w-64 h-64">
+      <h1>Score 7</h1>
+    </div>
+  )
+}
---
+++ components/tag.jsx
@@ -0,0 +1,7 @@
+export default function Tag({tag}) {
+  return (
+    <div className="rounded border border-solid border-gray-300 px-3 py-1 w-max mt-2">
+      <h1>{tag}</h1>
+    </div>
+  )
+}
