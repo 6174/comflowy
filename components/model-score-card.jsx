e30018650719d49eaa6ab0b346caf4a056d7cbf6
Jimmy Wong <thinkingjimmy@outlook.com>
2024-02-22 09:52:15 +0800

    fix download bug

--- components/model-score-card.jsx
+++ components/model-score-card.jsx
@@ -60,10 +60,10 @@ export default function ModelScoreCard({score,downloadlink, size}) {
         <div className="text-[#FFFFFF]/50">
           Size: {size}
         </div>
-        <button href={downloadlink} className="w-full p-2 rounded-lg bg-[#2ABDAE] text-white mt-2 flex items-center justify-center">
+        <a href={downloadlink} className="w-full p-2 rounded-lg bg-[#2ABDAE] text-white mt-2 flex items-center justify-center">
           <img src="/tray.and.arrow.down.svg" alt="Download Icon" className="mr-2" /> 
           {DOWNLOAD[locale]}
-        </button>
+        </a>
       </div>
     </div>
   )
