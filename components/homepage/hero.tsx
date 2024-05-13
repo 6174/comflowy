a1d8f3a3eb60b5639ea08f8c0d09e17862b9e31e
6174 <570171025@qq.com>
2024-05-13 16:01:02 +0800

    css background grid

---
+++ components/homepage/hero.tsx
@@ -0,0 +1,17 @@
+export function HeroBlock() {
+  return (
+    <div className="hero-block">
+      <LineBg/>
+      <div className="title">Hero Block</div>
+    </div>
+  )
+}
+
+function LineBg() {
+  return (
+    <div className="bg">
+      <div className="content">
+      </div>
+    </div>
+  )
+}
