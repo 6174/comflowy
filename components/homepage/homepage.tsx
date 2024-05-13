a7318272ac0f45518dc6440aed5ac4c98be31d4a
6174 <570171025@qq.com>
2024-05-13 16:00:50 +0800

    css background grid

--- components/homepage/homepage.tsx
+++ components/homepage/homepage.tsx
@@ -1,8 +1,9 @@
+import { HeroBlock } from "./hero";
 import styles from "./homepage.module.scss";
 export function HomePage() {
   return (
     <div className={styles.homepage}>
-      <div className="title">HomePage</div>
+      <HeroBlock />
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import type { AppProps } from 'next/app'
+import { useEffect} from "react";
 import 'nextra-theme-docs/style.css'
 import "../styles/global.scss";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { useRouter } from 'next/router';
 
 export default function App ({ Component, pageProps }) {
+  const router = useRouter();
+  const path = router.pathname;
+  useEffect(() => {
+    if (window) {
+      const realPath = path.replace(".en-US", "").replace('.zh-CN', "");
+      document.documentElement.dataset.page = realPath;
+    }
+  }, [path])
   return (
     <>
       <GoogleAnalytics strategy="lazyOnload" />
