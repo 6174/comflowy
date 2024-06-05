d7ec672668c9abbf8eeb800f16342a1b8422ab25
chenxuejia <chenxuejia67@gmail.com>
2024-06-05 21:33:52 +0800

    add twak to website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import 'nextra-theme-docs/style.css'
 import "../styles/global.scss";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { useRouter } from 'next/router';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 export default function App ({ Component, pageProps }) {
   const router = useRouter();
@@ -18,6 +19,10 @@ export default function App ({ Component, pageProps }) {
     <>
       <GoogleAnalytics strategy="lazyOnload" />
       <Component {...pageProps} />
+      <TawkMessengerReact
+        propertyId="66602842981b6c564778b9d7"
+        widgetId="1hvjpqh9n"
+      />
     </>
   )
 }
